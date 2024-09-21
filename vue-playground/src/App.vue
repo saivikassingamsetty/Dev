0c00df7316d1d9012ad87f687903917b3baf8742
Sai Vikas Singamsetty <saivikas.singamsetty@gmail.com>
2024-09-21 19:53:56 +0530

    feat(vue): init new vue playground

---
+++ vue-playground/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="increment">Count is {{ count }}</button>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const count = ref(0);
+const increment = () => count.value++;
+</script>
