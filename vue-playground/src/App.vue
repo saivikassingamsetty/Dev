92400162e37bb862c2b4185950325b82279ba76c
saivikassingamsetty <saivikas.singamsetty@gmail.com>
2024-09-28 17:04:20 +0530

    class and style bindings

--- vue-playground/src/App.vue
+++ vue-playground/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <!-- <TemplateBasics /> -->
-    <ReactivityBasics />
+    <!-- <ReactivityBasics /> -->
+    <ClassStyleBindings />
   </div>
 </template>
 
 <script setup>
 import TemplateBasics from "./components/TemplateBasics.vue";
 import ReactivityBasics from "./components/ReactivityBasics.vue";
+import ClassStyleBindings from "./components/ClassStyleBindings.vue";
 </script>
---
+++ vue-playground/src/components/ClassStyleBindings.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- HTML Classes -->
+  <div :class="{ blueColor: shouldApply, redColor: !shouldApply }">Hi</div>
+  <div :class="{ 'green-color': shouldApply, redColor: !shouldApply }">Hi</div>
+  <!-- computed -->
+  <div :class="divClassList">Hi</div>
+  ------
+  <!-- Inline Styles -->
+  <div :style="{ color: shouldApply ? `blue` : `red` }">Hi</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let shouldApply = ref(true);
+const divClassList = computed(() => ({
+  "green-color": shouldApply,
+  redColor: !shouldApply,
+}));
+</script>
+
+<style>
+.blueColor {
+  color: blue;
+}
+
+.redColor {
+  color: red;
+}
+
+.green-color {
+  color: green;
+}
+</style>
