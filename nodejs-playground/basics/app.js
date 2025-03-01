530d31cab48180de60a62ff96eb53a557dc683b6
Sai Vikas Singamsetty <saivikas.singamsetty@gmail.com>
2025-03-02 01:47:46 +0530

    Routing

--- nodejs-playground/basics/app.js
+++ nodejs-playground/basics/app.js
@@ -1,8 +1,22 @@
 const http = require("http");
+const fs = require("fs");
+
+//Read the html page
+const html = fs.readFileSync("./index.html", "utf-8");
 
 // creating server
 const server = http.createServer((request, response) => {
-  response.end("Hello from Server");
+  const path = request.url;
+  if (path.toLocaleLowerCase() === "/home" || path == "/") {
+    response.end(html.replace("{{%CONTENT%}}", "You are in Home Page"));
+  } else if (path.toLocaleLowerCase() === "/about") {
+    response.end(html.replace("{{%CONTENT%}}", "You are in About Page"));
+  } else if (path.toLocaleLowerCase() === "/contact") {
+    response.end(html.replace("{{%CONTENT%}}", "You are in Contact Page"));
+  } else {
+    response.end(html.replace("{{%CONTENT%}}", "Error 404: Page Not Found"));
+  }
+
   console.log(`A new request recieved!`);
 });
 
