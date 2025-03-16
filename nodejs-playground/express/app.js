18d90c7e1c4b213a4eb46c12ddc56f5bb6ca9dfe
saivikassingamsetty <saivikas.singamsetty@gmail.com>
2025-03-16 14:11:21 +0530

    Basic RESTful API

--- nodejs-playground/express/app.js
+++ nodejs-playground/express/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import * as fs from "node:fs";
 
 const app = express();
+
+const movies = JSON.parse(fs.readFileSync("./data/movies.json"));
 
 app.get("/", (req, res) => {
   // sends text/html
@@ -11,7 +14,24 @@ app.get("/", (req, res) => {
   res.status(200).json({ name: "Vikas" });
 });
 
-app.post("/", () => {});
+// app.post("/", () => {});
+
+//creating a web api GET /api/v1/movies
+app.get("/api/v1/movies", (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+
+  try {
+    // const rs = fs.createReadStream("./data/movies.json");
+    // rs.pipe(res);
+
+    res.status(200).json({
+      status: "success",
+      data: { movies },
+    });
+  } catch (error) {
+    res.send(404).send("Movies data not found");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server has started");
